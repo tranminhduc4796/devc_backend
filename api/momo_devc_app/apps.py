a61b7f6f23d58d931cf8d149d9aee167a64e2519
DVerse <dverse@pop-os.localdomain>
2019-10-26 17:32:14 +0700

    Init Django framework

---
+++ api/momo_devc_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MomoDevcAppConfig(AppConfig):
+    name = 'momo_devc_app'
---
+++ api/momo_devc_app/tests.py
@@ -0,0 +1,5 @@
+from django.test import TestCase
+from .models import Transactions, Users, Shops
+from rest_framework.test import APITestcase, APIClient
+
+# Create your tests here.
